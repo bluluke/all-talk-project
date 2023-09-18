ed50da7821fbc7583e14260835f3bfdcb7025b2e
bluluke <bluluke@hotmail.com>
2023-09-18 17:39:55 +0100

    Update: server start.

--- app.js
+++ app.js
@@ -10,3 +10,7 @@ mongoose.connect(process.env.DATABASE_URL, {
 const db = mongoose.connection
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to database'))
+
+
+
+module.exports = app; 
