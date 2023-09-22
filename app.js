bbe57434a3294cd31e29c5f056a078e5595af337
bluluke <bluluke@hotmail.com>
2023-09-22 10:51:22 +0100

    Update App.js: remove connection to database.

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express(); 
 app.use(express.json());
-const mongoose = require('mongoose');
 const apiRouter = require("./routes/api-router");
-
 app.use("/api", apiRouter)
 
-mongoose.connect(process.env.DATABASE_URL, {
-    useUnifiedTopology: true,
-});
-const db = mongoose.connection
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to database'))
 
 
-module.exports = app; 
+
+
+module.exports = {app}; 
