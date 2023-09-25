c471c92819f657b8d4e750ae85c2713e1197fd01
bluluke <bluluke@hotmail.com>
2023-09-25 17:09:22 +0100

    Update/Test Pass GET /api/chats: return chats in chronological order.

--- controllers/chats-controller.js
+++ controllers/chats-controller.js
@@ -3,7 +3,7 @@ const {readChats} = require('../models/chats-model')
 exports.getChats = (req, res, next) => {
     readChats()
     .then((data) => {
-        res.json(data);
+        res.status(200).send( {chats: data});
     })
 }
 
