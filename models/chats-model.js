452fd431b6b1f21e66feffbe74aca6ea19aa14a1
bluluke <bluluke@hotmail.com>
2023-09-27 12:00:15 +0100

    Update GET /api/chats?from_date= &&to_date=: return chat documents dated within tiime frame indicated by parametric timestamp values.

--- models/chats-model.js
+++ models/chats-model.js
@@ -8,21 +8,25 @@ exports.readChats = async (fromDate, toDate) => {
         const database = await client.db('all-talk-project')
         const chatListCollection = await database.collection('chat-list');
         let chatListData;
-        if(fromDate) {
+        if(typeof fromDate === 'number' && !isNaN(fromDate) && typeof toDate === 'number' && !isNaN(toDate)) {
+            chatListData = await chatListCollection
+            .find({
+                'timeOfCreation.$timestamp.t': { $gte: fromDate, $lte: toDate }
+            }).toArray();
+        } else if(typeof fromDate === 'number' && !isNaN(fromDate)) {
             chatListData = await chatListCollection
             .find({
                 'timeOfCreation.$timestamp.t': { $gte: fromDate }
         }).toArray()
-    } else if(typeof toDate === 'number' && !isNaN(toDate)) {
-        chatListData = await chatListCollection
-        .find({
-            'timeOfCreation.$timestamp.t': { $lte: toDate}
+        } else if(typeof toDate === 'number' && !isNaN(toDate)) {
+            chatListData = await chatListCollection
+            .find({
+                'timeOfCreation.$timestamp.t': { $lte: toDate}
         }).toArray()
-    }
-    else {
-        chatListData = await chatListCollection
-            .find({}).toArray() 
-    }
+        } else {
+            chatListData = await chatListCollection
+                .find({}).toArray() 
+        }
         const compareByTimestamp = (chatA, chatB) => {
             const timeStampA = chatA.timeOfCreation["$timestamp"].t;
             const timeStampB = chatB.timeOfCreation["$timestamp"].t;
@@ -34,3 +38,42 @@ exports.readChats = async (fromDate, toDate) => {
         console.error('Error reading chats', error)
     }
 }
+
+
+
+// const { connectToDatabase } = require('../connection');
+// const mongoose = require('mongoose');
+
+// exports.readChats = async (fromDate, toDate) => {
+//     try {
+//         await connectToDatabase();
+//         const client = mongoose.connection.client;
+//         const database = await client.db('all-talk-project')
+//         const chatListCollection = await database.collection('chat-list');
+//         let chatListData;
+//         if(typeof fromDate === 'number' && !isNaN(fromDate)) {
+//             chatListData = await chatListCollection
+//             .find({
+//                 'timeOfCreation.$timestamp.t': { $gte: fromDate }
+//         }).toArray()
+//     } else if(typeof toDate === 'number' && !isNaN(toDate)) {
+//         chatListData = await chatListCollection
+//         .find({
+//             'timeOfCreation.$timestamp.t': { $lte: toDate}
+//         }).toArray()
+//     }
+//     else {
+//         chatListData = await chatListCollection
+//             .find({}).toArray() 
+//     }
+//         const compareByTimestamp = (chatA, chatB) => {
+//             const timeStampA = chatA.timeOfCreation["$timestamp"].t;
+//             const timeStampB = chatB.timeOfCreation["$timestamp"].t;
+//             return timeStampB - timeStampA;
+//         };
+//         chatListData.sort(compareByTimestamp);
+//         return chatListData;
+//     } catch (error) {
+//         console.error('Error reading chats', error)
+//     }
+// }
