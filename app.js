d1c584582ad00df774d6b2c74ed8a8e352a1ad10
bluluke <bluluke@hotmail.com>
2023-10-16 13:44:06 +0100

    Add Websocket Handling.

--- app.js
+++ app.js
@@ -10,7 +10,23 @@ const wss = expressWs.getWss();
 app.use(express.json());
 app.use(cors());  
 
+app.ws('/websocket', (ws, req) => {
 
+  console.log('WebSocket connected');
+  ws.on('message', (message) => {
+    console.log('Received message:', message);
+
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('WebSocket disconnected');
+  });
+});
 
 
 const apiRouter = require("./routes/api-router");
